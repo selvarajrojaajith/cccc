518834fb600c6e3f89e7d7d425a0890baf9ab372
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-02-21 16:10:19 +0530

    fact.c
    good

---
+++ factorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    unsigned long long factorial = 1;
+
+    printf("Enter an integer: ");
+    scanf("%d",&n);
+else
+{
+for(i=0;i<n;i++)
+{
+factorial=1;
+}
+}
+return 0
+}
